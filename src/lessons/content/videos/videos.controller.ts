bea9476729cc79e44b997006df2b5f533c77dc76
front.andrey.sh <front.andrey.sh@alldigitalads.com>
2020-11-25 17:21:27 +0200

    all but lesson

--- src/lessons/content/videos/videos.controller.ts
+++ src/lessons/content/videos/videos.controller.ts
@@ -1,4 +1,57 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+  Query,
+  UseGuards,
+  Delete
+} from "@nestjs/common";
+import {ApiTags} from "@nestjs/swagger";
+import {VideoService} from "./videos.service";
+import {CreateVideoDto} from "./dto/create-video.dto";
+import {PaginationQueryDto} from "../../../common/dto/pagination-query.dto";
+import {UpdateVideoDto} from "./dto/update-video.dto";
+import { AuthGuard } from "@nestjs/passport";
 
+@ApiTags('Videos')
 @Controller('videos')
-export class VideosController {}
+export class VideosController {
+  constructor(public readonly videoService: VideoService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() createVideoDto: CreateVideoDto) {
+    return this.videoService.create(createVideoDto);
+  }
+
+  @Get()
+  @UseGuards(AuthGuard())
+  findAll(@Query() paginationQuery: PaginationQueryDto) {
+    return this.videoService.findAll(paginationQuery);
+  }
+
+  @Get(':hash')
+  @UseGuards(AuthGuard())
+  findOne(@Param('hash') userHash: string) {
+    return this.videoService.findOne(userHash);
+  }
+
+  @Put(':hash')
+  @UseGuards(AuthGuard())
+  update(@Param('hash') hash: string,
+         @Body() updateVideoDto: UpdateVideoDto) {
+    return this.videoService.update(hash, updateVideoDto);
+  }
+
+  @Delete(':hash')
+  @UseGuards(AuthGuard())
+  @HttpCode(HttpStatus.NO_CONTENT)
+  delete(@Param('hash') hash: string) {
+    return this.videoService.delete(hash);
+  }
+}
--- src/lessons/content/videos/videos.service.ts
+++ src/lessons/content/videos/videos.service.ts
@@ -1,4 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable, NotFoundException} from '@nestjs/common';
+import {InjectRepository} from "@nestjs/typeorm";
+import {Repository} from "typeorm";
+import {PaginationQueryDto} from "../../../common/dto/pagination-query.dto";
+import {Video} from "./entities/video.entity";
+import {UpdateVideoDto} from "./dto/update-video.dto";
+import {CreateVideoDto} from "./dto/create-video.dto";
 
 @Injectable()
-export class VideosService {}
+export class VideoService {
+  constructor(
+    @InjectRepository(Video)
+    private readonly videoRepo: Repository<Video>
+  ) {}
+
+  create(createVideoDto: CreateVideoDto) {
+    const keyNote = this.videoRepo.create(createVideoDto);
+    return this.videoRepo.save(keyNote);
+  }
+
+  findAll(paginationQuery: PaginationQueryDto) {
+    const { limit, page } = paginationQuery;
+    return this.videoRepo.find({
+      skip: page,
+      take: limit,
+    });
+  }
+
+  async findOne(contentHash: string) {
+    const content = await this.videoRepo.findOne({hash: contentHash});
+    if (!content) {
+      throw new NotFoundException(`No such contentHash: ${contentHash} found`);
+    }
+    return content;
+  }
+
+  async update(videoHash: string, updateVideoDto: UpdateVideoDto) {
+    const video = await this.videoRepo.findOne({hash: videoHash});
+    if (!video) {
+      throw new NotFoundException(`No such videoHash: ${videoHash} found`);
+    }
+    const videoRepo = await this.videoRepo.preload({
+      hash: video.hash,
+      ...updateVideoDto,
+    });
+
+    return this.videoRepo.save(videoRepo);
+  }
+
+  async delete(hash: string) {
+    const content = await this.findOne(hash);
+    return this.videoRepo.remove(content);
+  }
+}
